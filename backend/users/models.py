1330b5aa7c5386d1362c2fce7b261e5051ed0bfc
Lucifer1147 <4n0nym0us1147@gmail.com>
2024-07-27 20:05:30 +0530

    Added the User Manager Model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('username',)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
