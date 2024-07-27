b89b5020a8e5d6426992cb5dcde50c019490a121
Lucifer1147 <4n0nym0us1147@gmail.com>
2024-07-27 23:32:51 +0530

    Updated the url patterns

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, re_path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^auth/', include('djoser.urls')),
-    re_path(r'^auth/', include('djoser.urls.jwt'))
+    re_path(r'^api/v1/auth/', include('djoser.urls')),
+    re_path(r'^api/v1/auth/', include('djoser.urls.jwt'))
 ]
 
