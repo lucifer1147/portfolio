153af00e4592265aa48f1c178bb57d20c2305633
Lucifer1147 <4n0nym0us1147@gmail.com>
2024-07-27 20:22:19 +0530

    Added fields like is_staff, is_active and date_joined in the UserManager

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,18 +1,21 @@
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext as _
 
 from .managers import CustomUserManager
 
 
-class CustomUser(AbstractBaseUser):
-    email = models.EmailField(_('email address'), unique=True)
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_("email address"), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ('username',)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
 
     def __str__(self):
         return self.email
-
