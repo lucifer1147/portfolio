ed4e75a9198ba05f7f7f3c39225b140c28d94a87
Lucifer1147 <4n0nym0us1147@gmail.com>
2024-07-28 16:35:18 +0530

    Laid out the basic structure so that i may start working

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import {active} from '$lib/stores.js';
+    active.set('home')
+</script>
+
+<div class="flex justify-center items-center h-[90%]">
+    <div class="flex flex-wrap items-center justify-center h-[90%] w-2/3">
+        <div class="pl-20">
+            <p class="text-[#909090] drop-shadow-lg text-md">I'm a</p>
+            <h1 class="text-4xl mb-10 text-teal-500 drop-shadow-lg w-1/2">Full Stack Developer</h1>
+            <p class="text-white font-semibold text-2xl shadow-amber-800 w-1/2">
+                Welcome to my portfolio! I'm a full-stack developer specializing in Django (Python) and SvelteKit!
+            </p>
+
+            <div class="pt-32">
+                <button class="bg-teal-400 text-xl px-6 py-3 rounded-l-lg w-1/4">Sign Up</button>
+                <button class="bg-teal-400 text-xl px-6 py-3 rounded-r-lg w-1/4">Login</button>
+            </div>
+
+            <div class="flex w-1/2 justify-center items-center text-[#909090]">
+                <hr class="w-full">
+                OR
+                <hr class="w-full">
+            </div>
+
+            <div>
+                <button class="bg-teal-900 text-xl text-gray-400 px-6 py-3 rounded-lg w-1/2">Continue as Guest</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="pr-32">
+
+    </div>
+</div>
