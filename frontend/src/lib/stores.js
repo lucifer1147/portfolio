3abe85ec4969bf91dae2c71e2b149a6855f5e69b
Lucifer1147 <4n0nym0us1147@gmail.com>
2024-07-29 14:16:05 +0530

    Added writables for username and email of the user with appropriate default values

--- frontend/src/lib/stores.js
+++ frontend/src/lib/stores.js
@@ -2,3 +2,6 @@ import {writable} from "svelte/store";
 
 export const active = writable('home')
 export const apiUrl = writable('http://localhost:8000/api/v1/auth/')
+
+export const username = writable('Guest')
+export const email = writable('')
