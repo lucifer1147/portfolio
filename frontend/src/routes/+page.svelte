20acb697f3b589b934868236e8149d53c890187b
Lucifer1147 <4n0nym0us1147@gmail.com>
2024-07-29 15:29:51 +0530

    reformatted the file

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import {active} from '$lib/stores.js';
     import {isLoggedIn} from "$lib/commonfuncs.js";
+    import {goto} from "$app/navigation";
 
     let loggedIn;
     isLoggedIn().then((v) => loggedIn = v)
@@ -19,13 +20,18 @@
 
             <div class="pt-32">
                 {#if loggedIn}
-                    <button class="bg-teal-400 text-xl px-6 py-3 rounded-lg w-1/2"><a
-                            href="/dashboard">Go to Dashboard</a></button>
+                    <button class="bg-teal-400 text-xl h-16 rounded-lg w-1/2" on:click={()=>goto('/dashboard')}>
+                        Go to Dashboard
+                    </button>
                 {:else}
-                    <button class="bg-teal-400 text-xl px-6 py-3 rounded-l-lg w-1/4 border-r-2 border-white"><a
-                            href="/api/v1/auth/signup">Sign Up</a></button>
-                    <button class="bg-teal-400 text-xl px-6 py-3 rounded-r-lg w-1/4 -translate-x-1 border-l-2 border-white">
-                        <a href="/api/v1/auth/login">Login</a></button>
+                    <button class="bg-teal-400 text-xl px-6 py-3 rounded-l-lg w-1/4 border-r-2 border-white"
+                            on:click={()=>goto('/api/v1/auth/signup')}>
+                        Sign Up
+                    </button>
+                    <button class="bg-teal-400 text-xl px-6 py-3 rounded-r-lg w-1/4 -translate-x-1 border-l-2 border-white"
+                            on:click={()=>goto('/api/v1/auth/login')}>
+                        Login
+                    </button>
                 {/if}
             </div>
 
