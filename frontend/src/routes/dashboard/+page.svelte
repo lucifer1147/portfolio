a130e0265041e79bc4b4d1d7a62e598a7c0e0d8c
Lucifer1147 <4n0nym0us1147@gmail.com>
2024-07-29 15:06:13 +0530

    Laid out a basic structure

---
+++ frontend/src/routes/dashboard/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let data;
+
+    import {isLoggedIn} from "$lib/commonfuncs.js";
+    import {username, email, active} from "$lib/stores.js";
+    import {onMount} from "svelte";
+
+    active.set('dashboard')
+    username.set(data.username)
+    email.set(data.email)
+
+    let res;
+    onMount(async () => {
+        res = await isLoggedIn();
+        console.log(res);
+    })
+
+</script>
