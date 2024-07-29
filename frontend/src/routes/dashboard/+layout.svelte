8f947c7c8fb845b8e435f54b1923d6e16d223a06
Lucifer1147 <4n0nym0us1147@gmail.com>
2024-07-29 15:35:25 +0530

    Added the logic such that if the user is not logged in he/she is redirected to the home page

---
+++ frontend/src/routes/dashboard/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    import {isLoggedIn} from "$lib/commonfuncs.js";
+    import {goto} from "$app/navigation";
+    import {browser} from "$app/environment";
+
+    if (browser)
+    isLoggedIn().then((v) => {
+        if (!v) goto('/')
+    })
+
+
+</script>
+<slot />
